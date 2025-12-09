65fa40d52ed10be462cf1e3163e86a774f182857
Omar <ahlawyomar95@gmail.com>
2025-12-09 04:08:13 +0200

    add folders

---
+++ numbers/numbers.py
@@ -0,0 +1,28 @@
+# including a library
+from math import *
+
+#basic operations
+print(1+2)
+print(1-2)
+print(1*2)
+print(1/2)
+print(1%2)
+
+#var
+num =5
+print(num)
+print(str(num) + " my fav number")
+
+#some fuctions
+Num = -4
+print(abs(Num)) #convert negative to postive
+print(pow(2,3)) # 2^3
+print(max(2,8)) #maximum between two numbers
+print(min(2,8)) #minimum between two numbers
+print(round(3.2)) # --> 3
+print(round(3.7)) # --> 4
+
+# using from math import *
+print(floor(3.7))  # --> 3
+print(ceil(3.7))  # --> 4
+print(sqrt(9))  # --> 3
