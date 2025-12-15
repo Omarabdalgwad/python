4da024b26ab851d765690b9b938689bb39e5ab98
Omar <ahlawyomar95@gmail.com>
2025-12-15 05:29:54 +0200

    add

---
+++ 15- while loop/while loop.py
@@ -0,0 +1,27 @@
+## with continue
+
+i = 1
+
+while i <= 10:
+    i+=1
+    if i == 6:
+        continue
+    print(i)
+      
+    
+else:
+    print("the condition is not true")
+
+## with break
+
+i = 1
+
+while i <= 10:
+    i+=1
+    if i == 6:
+        break
+    print(i)
+      
+    
+else:
+    print("the condition is not true")
